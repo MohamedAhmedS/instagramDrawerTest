e6243af373f5f883d874d4cd7c86ba59c72cd4e7
NiMiCiK <k0rv3x@gmail.com>
2020-10-17 23:23:10 +0400

    hi

--- app/src/main/java/com/instagramdrawertest/MainActivity.java
+++ app/src/main/java/com/instagramdrawertest/MainActivity.java
@@ -1,10 +1,23 @@
 package com.instagramdrawertest;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -16,11 +29,12 @@ import android.widget.RelativeLayout;
 public class MainActivity extends AppCompatActivity {
 
     private ViewGroup bigParent;
-    private ImageView burger;
+    private ImageView burger, imageView, backgroundImage;
     private LinearLayout right_sheet;
     private RelativeLayout holder1;
     private Context context;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +44,17 @@ public class MainActivity extends AppCompatActivity {
         burger = findViewById(R.id.burger);
         right_sheet = findViewById(R.id.right_sheet);
         holder1 = findViewById(R.id.holder1);
+        imageView = findViewById(R.id.imageView);
+        backgroundImage = findViewById(R.id.backgroudImage);
 
+        imageView.setImageResource(R.mipmap.gold);
+        imageView.setClipToOutline(true);
+        backgroundImage.setImageResource(R.mipmap.gold);
+        backgroundImage.setClipToOutline(true);
+
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.gold);
+//        imageView.setImageBitmap(getRoundedCornerBitmap(bitmap, 360));
+//which adds the imageview to your layout
 //        final int scale = (int) context.getResources().getDisplayMetrics().density;
 //        final TypedArray array = context.getTheme().obtainStyledAttributes(
 //                new int[] { android.R.attr.actionBarSize });
@@ -114,4 +138,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
+                .getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = pixels;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
 }
